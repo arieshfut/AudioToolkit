febd858f3709cdb0a33e12f17f142167fec43d6a
郑亚军 <zhengyajun@cmhi.chinamobile.com>
2023-09-25 14:17:38 +0800

    [bugfix] rm unused dir and file.

--- app/src/main/java/com/mindlinker/audiotoolkit/AudioModule/AudioBase.java
+++
@@ -1,8 +0,0 @@
-package com.mindlinker.audiotoolkit.AudioModule;
-
-public class AudioBase {
-    public static int FLAG_NONE = 0X0;
-    public static int FLAG_RECORD = 0X01;
-    public static int FLAG_PLAY = 0X10;
-    public static int FLAG_SHARE = 0X100;
-}
